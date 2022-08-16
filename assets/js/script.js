86bf3324f002c84e42f7a9c4a6f2296e72cf5dc8
NickWaldock <nickwaldock@me.com>
2022-08-16 20:07:22 +0000

    create new audio const for fx samples

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 // Sounds 
 bassDrum = new Audio('./assets/audio/no-fx/bass-drum.wav', './assets/audio/no-fx/bass-drum.mp3');
+bassDrumFx = new Audio('./assets/audio/fx/bass-drum-fx.wav', './assets/audio/fx/bass-drum-fx.mp3');
 snareDrum = new Audio('./assets/audio/no-fx/snare-drum.wav', './assets/audio/no-fx/snare-drum.mp3');
+snareDrumFx = new Audio('./assets/audio/fx/snare-drum-fx.wav', './assets/audio/fx/snare-drum-fx.mp3');
 hats = new Audio('./assets/audio/no-fx/hats.wav', './assets/audio/no-fx/hats.mp3');
+hatsFx = new Audio('./assets/audio/fx/hats-fx.wav', './assets/audio/fx/hats-fx.mp3');
 crash = new Audio('./assets/audio/no-fx/crash.wav', './assets/audio/no-fx/crash.mp3');
+crashFx = new Audio('./assets/audio/fx/crash-fx.wav', './assets/audio/fx/crash-fx.mp3');
 highTom = new Audio('./assets/audio/no-fx/high-tom.wav', './assets/audio/no-fx/high-tom.mp3');
+highTomFx = new Audio('./assets/audio/fx/high-tom-fx.wav', './assets/audio/fx/high-tom-fx.mp3');
 lowTom = new Audio('./assets/audio/no-fx/low-tom.wav', './assets/audio/no-fx/low-tom.mp3');
+lowTomFx = new Audio('./assets/audio/fx/low-tom-fx.wav', './assets/audio/fx/low-tom-fx.mp3');
 
 // DOM Elements - Icon Triggers
 const bassDrumTrigger = document.getElementById('bass-drum');
